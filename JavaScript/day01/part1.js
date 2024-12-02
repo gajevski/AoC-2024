b8da2fafbd5290b9cf231075749b564420be3ad2
gajevski <mikolajxgajewski@gmail.com>
2024-12-02 17:02:21 +0100

    js: day01 part01

--- JavaScript/day01/part1.js
+++ JavaScript/day01/part1.js
@@ -3,9 +3,26 @@ const fs = require('fs');
 
 function solve() {
   const filePath = "./input.txt";
-  const data = fs.readFileSync(filePath, 'utf-8').trim().split('\r\n');
+  const lines = fs.readFileSync(filePath, 'utf-8').trim().split('\r\n');
+  const leftArray = [];
+  const rightArray = [];
+  let sum = 0;
+  lines.forEach(line => {
+    const [left, right] = line.trim().split(/\s+/).map(Number);
+    leftArray.push(left);
+    rightArray.push(right);
+  });
 
-  return data;
+  leftArray.forEach((_, i) => {
+    const sortedLeft = leftArray.toSorted();
+    const sortedRight = rightArray.toSorted();
+    let lowest = Math.min(sortedLeft[i], sortedRight[i]);
+    let highest = Math.max(sortedLeft[i], sortedRight[i]);
+    let subDif = highest - lowest;
+    sum += subDif;
+  })
+
+  return sum;
 }
 
 console.log(solve());
